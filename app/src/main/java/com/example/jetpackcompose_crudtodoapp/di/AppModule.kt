db16f33274a0334919ecb46f3820637998b7a37e
Orhan Uckulac <orhanuckulac0@gmail.com>
2023-01-10 21:32:53 +0300

    3. commit - implement dependency injection for db and repository

---
+++ app/src/main/java/com/example/jetpackcompose_crudtodoapp/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.example.jetpackcompose_crudtodoapp.di
+
+import androidx.room.Room
+import com.example.jetpackcompose_crudtodoapp.TodoApp
+import com.example.jetpackcompose_crudtodoapp.data.TodoDatabase
+import com.example.jetpackcompose_crudtodoapp.data.TodoRepository
+import com.example.jetpackcompose_crudtodoapp.data.TodoRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(app: TodoApp): TodoDatabase{
+        return Room.databaseBuilder(
+            app,
+            TodoDatabase::class.java,
+            "todo_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(db: TodoDatabase): TodoRepository{
+        return TodoRepositoryImpl(db.todoDAO)
+    }
+}
