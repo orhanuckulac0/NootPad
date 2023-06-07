0ce1cd611d843793e1bc3af6d61c18e2f25be0b4
Orhan Uckulac <orhanuckulac0@gmail.com>
2023-06-07 04:00:47 +0300

    30. commit - fix bug on themes theme was stuck on dark, make it light theme on all devices

--- app/src/main/java/com/example/jetpackcompose_crudtodoapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetpackcompose_crudtodoapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.jetpackcompose_crudtodoapp.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
@@ -30,14 +29,9 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun JetpackComposeCRUDTodoAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
